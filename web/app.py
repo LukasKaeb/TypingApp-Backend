8d902307e7140e5219a92cbeb02f3c637e7786e1
Lukas <149614931+LukasKaeb@users.noreply.github.com>
2024-10-30 01:28:56 +0100

    CORS update

--- web/app.py
+++ web/app.py
@@ -4,7 +4,7 @@ from flask_restful import Api, Resource
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "*"}})
 api = Api(app)
 
 client = MongoClient('mongodb://db:27017')
