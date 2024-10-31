0e3aa630ec87adb1a67448738ef8e9872a29bba5
Lukas <149614931+LukasKaeb@users.noreply.github.com>
2024-10-31 01:29:59 +0100

    CORS changes

--- web/app.py
+++ web/app.py
@@ -4,7 +4,7 @@ from flask_restful import Api, Resource
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "http://localhost:5173"}})
+CORS(app)
 api = Api(app)
 
 client = MongoClient('mongodb://db:27017')
@@ -112,7 +112,6 @@ api.add_resource(UpdateTestCount, '/update_test_count')
 api.add_resource(AddUser, '/add_user')       
 api.add_resource(UpdateTimeTyping, '/update_time_typing')
 api.add_resource(StoreTestResult, '/store_test_result')
-       
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
